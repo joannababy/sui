538e63dda2a0985cf1bf24efe5de3987e619d9fd
classicemi <wushuang_1227@163.com>
2014-09-26 15:45:30 +0800

    test/dialog/dialog-test.js

--- test/dialog/dialog-test.js
+++ test/dialog/dialog-test.js
@@ -18,9 +18,28 @@ define(function(require) {
                 d = new Dialog({
                     content: 'test1'
                 });
-                // console.log(d)
                 d.show();
                 expect(d._find('content').html()).to.be('test1');
+            });
+
+            it('is dom', function() {
+                var $dom = $('<div id="test2">test2</div>');
+                d = new Dialog({
+                    content: $dom
+                });
+                d.show();
+
+                var test = d._find('content').children().eq(0);
+                expect(test.attr('id')).to.be('test2');
+                expect(test.html()).to.be('test2');
+            });
+
+            it('is html', function() {
+                d = new Dialog({
+                    content: '<h1>test3</h1>'
+                });
+                d.show();
+                expect(d._find('content').html()).to.be('<h1>test3</h1>');
             });
 
         });
