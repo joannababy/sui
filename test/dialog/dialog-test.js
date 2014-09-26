9854d91fe849b70ffca39f82788e0323479fa5dc
classicemi <wushuang_1227@163.com>
2014-09-26 11:23:59 +0800

    modify test

--- test/dialog/dialog-test.js
+++ test/dialog/dialog-test.js
@@ -2,8 +2,27 @@ define(function(require) {
     var Dialog = require('dialog');
 
     describe('dialog', function() {
-        it('test', function() {
+        var d;
+
+        afterEach(function() {
+            if (d) {
+                d.hide();
+                d.destroy();
+                d = null;
+            }
+        });
+
+        describe('content', function() {
             
+            it('is string', function() {
+                d = new Dialog({
+                    content: 'test1'
+                });
+                // console.log(d)
+                d.show();
+                expect(d._find('content').html()).to.be('test1');
+            });
+
         });
     });
 });
